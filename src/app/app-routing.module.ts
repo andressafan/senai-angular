47368a08c10c381c939ece4b49e667818b3d7c24
Andressa <andressafelix@outlook.com>
2024-02-28 16:14:25 -0300

    atualizações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { RestritoComponent } from './restrito/restrito.component';
+import { GuardGuard } from './guard.guard';
 
 const routes: Routes = [
   {path:'inicio', component: InicioComponent},
   {path:'login', component: LoginComponent},
   {path:'cadastro', component: CadastroComponent},
-  {path: 'restrito', component: RestritoComponent},
+  {path: 'restrito', component: RestritoComponent, canActivate: [GuardGuard]},
   {path:'', redirectTo:'/inicio', pathMatch: 'full'}
 ];
 
